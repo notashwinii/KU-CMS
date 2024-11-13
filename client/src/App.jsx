55dd6140f48c956eb832317e570b04b109117536
Kapil Adhikari <kapiladhikari2060@gmail.com>
2024-11-13 14:09:04 +0545

    Login Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import React from 'react'
+
+// Routes
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
+// Pages
+import LoginPage from './pages/login/LoginPage';
+
+// Components
 
 function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element = {<LoginPage/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App
+export default App;
---
+++ client/src/pages/login/LoginPage.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+// Styles
+import "./LoginStyles.css";
+
+const LoginPage = () => {
+  return (
+    <>
+      <div className="loginContainer" id="container">
+        <div className="form-container sign-up">
+          <form>
+            <img src="KU-CMS.png" alt="KU-CMS" className="logo"/>
+            <h2>Teacher Login</h2>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+        <div className="form-container sign-in">
+          <form>
+            <img src="KU-CMS.png" alt="KU-CMS" className="logo"/>
+            <h2>Student Login</h2>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+        <div className="toggle-container">
+          <div className="toggle">
+            <div className="toggle-panel toggle-left">
+              <h2>Are you a student?</h2>
+              <p>
+                Login your student account to access your courses and
+                assignments.
+              </p>
+              <button
+                className="hidden"
+                onClick={() => {
+                  container.classList.remove("active");
+                }}
+              >
+                Login as Student
+              </button>
+            </div>
+            <div className="toggle-panel toggle-right">
+              <h2>Are you a Teacher?</h2>
+              <p>
+                Login your teacher account to manage your classes and students.
+              </p>
+              <button
+                className="hidden"
+                onClick={() => {
+                  container.classList.add("active");
+                }}
+              >
+                Login as Teacher
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
